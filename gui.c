07f8eca3559d3e2a45f125a3d2dd0162ec12795d
Martin Grabina <mgrabina@itba.edu.ar>
2018-01-29 18:05:36 -0300

    Little Changes

--- gui.c
+++ gui.c
@@ -21,17 +21,36 @@ void menu(){
 }
 
 void flightStatus(){
-
+	int flightNumberAsked;
+	printf("Please enter the flight number.\n");
+	scanf("%d", flightNumberAsked);
+	//Print Status
 }
 
 void makeReservation(){
-
+	printf("Please select a flight.\n");
+	//Print Flights
 }
 
 void removeReservation(){
+	int reservationNumberAsked;
+	printf("Please enter the reservation number.\n");
+	scanf("%d", reservationNumberAsked);
+	//Remove Reservation
 
 }
 
 void manageflights(){
-
+	int flightNumberAsked;
+	printf("Please enter the flight number.\n");
+	scanf("%d", flightNumberAsked);
+	//Print Info or Error Message
+	printf(" (1) Modify\n (2) Remove\n");
+	char selected = getchar(); 
+	switch(selected){
+		case '1': //Modify Flight Info
+			break;
+		case '2': //Remove Flight
+			break;
+	}
 }
